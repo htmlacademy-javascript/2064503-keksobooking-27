b47d569aad8de5ff57dc05f960c1cc7c6d9d1676
Grony <gr0ny.0017@gmail.com>
2022-10-02 18:39:38 +0300

    Добавляет функции

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function getRandomNumbery(min, max) {
+  if (checkingCorrectData(min, max)) {
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+  }
+  return NaN;
+}
+
+function getRandomFractionalNumber(min, max, decimalPlaces) {
+  if (checkingCorrectData(min, max) && (decimalPlaces == +decimalPlaces)) {
+    return (Math.random() * (max - min) + min).toFixed(decimalPlaces);
+  }
+  return NaN;
+}
+
+function checkingCorrectData(min, max) {
+  if (min > max) {
+    return false;
+  }
+  return true;
+}
+
+console.log(getRandomNumbery(2, 10));
+console.log(getRandomFractionalNumber(2, 10, 6));
